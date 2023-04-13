5addebdb31a2037c01c8f735d350c5b5a2fdb522
Nenad Jovanovic <nenad.jovanovic@crowdbotics.com>
2023-04-13 12:07:36 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "pricing-review-nena-38187",
-  url: "https://pricing-review-nena-38187.botics.co",
-  api: "https://pricing-review-nena-38187.botics.co/api/v1"
-}
+  "name": "pricing-review-nena-38187",
+  "url": "https://pricing-review-nena-38187.botics.co",
+  "api": "https://pricing-review-nena-38187.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
